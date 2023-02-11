f065c8cbabc31c0c0495c568a9ff41ea5cbfeb4f
Jaloliddin <122007173+Jaloliddinnet@users.noreply.github.com>
2023-02-11 19:21:35 +0500

    CarAdd

--- WebApplicationGitHub/Controllers/CarController.cs
+++ WebApplicationGitHub/Controllers/CarController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data.Models;
 
 namespace WebApplicationGitHub.Controllers
 {
@@ -7,5 +9,25 @@ namespace WebApplicationGitHub.Controllers
     [ApiController]
     public class CarController : ControllerBase
     {
+        AppDbContext context;
+        public CarController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await context.cars.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm]Car car)
+        {
+            await context.cars.AddAsync(car);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }

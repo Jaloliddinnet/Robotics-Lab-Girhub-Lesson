07228bd8e149a38746208cfc550a796530cfe5f1
abbbosee <abbosxon.isomiddinov@bk.ru>
2023-02-11 19:25:01 +0500

    MealsControllerIsReady

--- Web.Data/AppDbContext.cs
+++ Web.Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace WebApplicationGitHub
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
-        DbSet<Car> cars { get; set; }
+
+        public DbSet<Meals> meals { get; set; }      
     }
 }
---
+++ WebApplicationGitHub/Controllers/MealsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data.Models;
+
+namespace WebApplicationGitHub.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        AppDbContext db;
+       
+        public MealsController(AppDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await db.meals.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Meals meal)
+        {
+            await db.meals.AddAsync(meal);
+            await db.SaveChangesAsync();
+            return Ok("Done");
+        }
+
+    }
+}
